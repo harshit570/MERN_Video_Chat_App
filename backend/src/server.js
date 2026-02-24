ab32f31553d8a57f5fb1bb865c087cd99f9d7afe
harshit570 <harshitsingh18032005@gmail.com>
2026-02-24 18:33:43 +0530

    tanstack query setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 // import "dotenv/config"; // Alternative way to load environment variables
 dotenv.config();
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import chatRoutes from './routes/chat.route.js';
@@ -12,6 +13,10 @@ import { connectDB } from './lib/db.js';
 const app=express();
 const PORT=process.env.PORT;
 
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true   // allow frontend to send the cookies
+}));
 app.use(express.json());
 app.use(cookieParser());
 
