bcf92ec4ef668c0f615e6d55d8f06587a427b3ea
harshit570 <harshitsingh18032005@gmail.com>
2026-02-27 20:03:11 +0530

    Themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,10 +11,13 @@ import OnboardingPage from "./pages/OnboardingPage.jsx";
 import { Toaster } from "react-hot-toast";
 import PageLoader from "./components/PageLoader.jsx";
 import useAuthUser from "./hooks/useAuthUser.js";
+import Layout from "./components/Layout.jsx";
+import { useThemeStore } from "./store/useThemeStore.js";
 
 const App = () => {
   // react query tanstack query
   const { isLoading, authUser } = useAuthUser();
+  const {theme}=useThemeStore();
 
   const isAuthenticated = Boolean(authUser);
   const isOnboarded = authUser?.isOnboarded;
@@ -22,13 +25,15 @@ const App = () => {
   if (isLoading) return <PageLoader />;
 
   return (
-    <div className=" h-screen " data-theme="night">
+    <div className=" h-screen " data-theme={theme}>
       <Routes>
         <Route
           path="/"
           element={
             isAuthenticated && isOnboarded ? (
-              <HomePage />
+              <Layout showSidebar={true}>
+                <HomePage />
+              </Layout>
             ) : (
               <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
             )
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,41 @@
-import React from 'react'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import { getOutgoingFriendReqs, getRecommendedUsers, getUserFriends, sendFriendRequest } from '../lib/api';
 
 const HomePage = () => {
+  const queryClient=useQueryClient();
+  const [outgoingRequestsIds,setOutgoingRequestsIds]=useState(new Set());
+
+  const {data:friends=[],isLoading:loadingFriends} = useQuery({
+    queryKey:["friends"],
+    queryFn:getUserFriends
+  })
+
+  const {data:recommendedUsers=[],isLoading:loadingUsers} = useQuery({
+    queryKey:["users"],
+    queryFn:getRecommendedUsers
+  })
+
+  const {data:outgoingFriendReqs}=useQuery({
+    queryKey:["outgoingFriendReqs"],
+    queryFn:getOutgoingFriendReqs,
+  })
+
+  const {data:sendRequestMutation,isPending}=useMutation({
+    mutationFn:sendFriendRequest,
+    onSuccess:()=>queryClient.invalidateQueries({queryKey:["outgoingFriendReqs"]})
+  })
+
+useEffect(() => {
+    const outgoingIds = new Set();
+    if (outgoingFriendReqs && outgoingFriendReqs.length > 0) {
+      outgoingFriendReqs.forEach((req) => {
+        outgoingIds.add(req.id);
+      });
+      setOutgoingRequestsIds(outgoingIds);
+    }
+  }, [outgoingFriendReqs]);
+ 
   return (
     <div>HomePage</div>
   )
