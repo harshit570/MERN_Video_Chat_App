b487051ae769266a5b6449aca16bff09bcc09407
harshit570 <harshitsingh18032005@gmail.com>
2026-03-02 20:31:58 +0530

    video-Call

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -72,8 +72,16 @@ const App = () => {
           }
         />
         <Route
-          path="/call"
-          element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />}
+          path="/call/:id"
+          element={
+            isAuthenticated && isOnboarded ? (
+              <Layout showSidebar={false}>
+                <CallPage />
+              </Layout>
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
         />
         <Route
           path="/chat/:id"
